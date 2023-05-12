dce78431088093ee45ab6d01dc0b24dde857f784
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-05-11 22:07:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -48,10 +48,6 @@ function handleProgress(){
     progressBar.style.flexBasis = `${percent}%`
 }
 
-// function updateRangeTooltip() {
-//     this.setAttribute('title', `${this.title} - ${this.value}`);
-// }
-
 function scrub(e){
     const scrubTime = (e.offsetX / progress.offsetWidth ) * video.duration;
     video.currentTime = scrubTime;
